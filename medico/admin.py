e12c43ad95c65bac2296ffe4fc4137884f0687db
Jonas Thiago <146122741+jonas-thiago@users.noreply.github.com>
2024-04-19 16:56:03 -0300

    version2
    realizado as partes iniciais das páginas de consultas do paciente e do médico

--- medico/admin.py
+++ medico/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Especialidades, DadosMedicos
+from .models import Especialidades, DadosMedicos, DatasAbertas
 
 # Register your models here.
 
 admin.site.register(Especialidades)
 admin.site.register(DadosMedicos)
+admin.site.register(DatasAbertas)
