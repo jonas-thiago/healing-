6854fd094f445fa76dbb18b0de8184c7615da9fb
Jonas Thiago <146122741+jonas-thiago@users.noreply.github.com>
2024-04-20 15:17:33 -0300

    versio3.0
    partials para a barra de navegação concluido

--- medico/views.py
+++ medico/views.py
@@ -20,7 +20,7 @@ def cadastro_medico(request):
     if request.method == 'GET':
         especialidades = Especialidades.objects.all()
 
-        return render(request, 'cadastro_medico.html', {'especialidades': especialidades})
+        return render(request, 'cadastro_medico.html', {'especialidades': especialidades, 'is_medico': is_medico(request.user)})
     
     elif request.method == 'POST':
         crm = request.POST.get('crm')
@@ -70,7 +70,7 @@ def abrir_horario(request):
     if request.method == 'GET':
         dados_medicos = DadosMedicos.objects.get(user=request.user)
         datas_abertas = DatasAbertas.objects.filter(user=request.user)
-        return render(request, 'abrir_horario.html', {'dados_medicos': dados_medicos, 'datas_abertas': datas_abertas})
+        return render(request, 'abrir_horario.html', {'dados_medicos': dados_medicos, 'datas_abertas': datas_abertas, 'is_medico': is_medico(request.user)})
     
     elif request.method == 'POST':
         data = request.POST.get('data')
--- paciente/views.py
+++ paciente/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from medico.models import DadosMedicos, Especialidades, DatasAbertas
+from medico.models import DadosMedicos, Especialidades, DatasAbertas, is_medico
 from django.http import HttpResponse
 from datetime import datetime
 from .models import Consulta
@@ -21,7 +21,7 @@ def home(request):
             medicos = medicos.filter(especialidade_id__in=especialidades_filtrar)
 
         especialidades = Especialidades.objects.all()
-        return render(request, 'home.html', {'medicos': medicos, 'especialidades': especialidades})
+        return render(request, 'home.html', {'medicos': medicos, 'especialidades': especialidades, 'is_medico': is_medico(request.user)})
 
 
 
@@ -29,7 +29,7 @@ def escolher_horario(request, id_dados_medicos):
     if request.method == "GET":
         medico = DadosMedicos.objects.get(id=id_dados_medicos)
         datas_abertas = DatasAbertas.objects.filter(user=medico.user).filter(data__gte=datetime.now()).filter(agendado=False)
-        return render(request, 'escolher_horario.html', {'medico': medico, 'datas_abertas': datas_abertas})
+        return render(request, 'escolher_horario.html', {'medico': medico, 'datas_abertas': datas_abertas, 'is_medico': is_medico(request.user)})
     
 
 def agendar_horario(request, id_data_aberta):
@@ -54,4 +54,4 @@ def minhas_consultas(request):
     if request.method == "GET":
         
         minhas_consultas = Consulta.objects.filter(paciente=request.user).filter(data_aberta__data__gte=datetime.now())
-        return render(request, 'minhas_consultas.html', {'minhas_consultas': minhas_consultas})
+        return render(request, 'minhas_consultas.html', {'minhas_consultas': minhas_consultas, 'is_medico': is_medico(request.user)})
